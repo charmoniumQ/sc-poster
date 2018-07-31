bbd75d833e9217a55daca468fe612a5188d3cb8a
Samuel Grayson <sam@samgrayson.me>
2018-07-31 14:41:34 -0500

    added malloc.png source code

---
+++ plots/common.py
@@ -0,0 +1,6 @@
+import matplotlib
+matplotlib.use('Agg')
+
+matplotlib.rc('font', **{'family': 'serif', 'serif': ['cm'], 'size': 12})
+matplotlib.rc('text', **{'usetex': True})
+matplotlib.rc('savefig', **{'dpi': 600})
